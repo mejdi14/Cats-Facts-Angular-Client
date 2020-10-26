93a93560b5b7c62095a29b69155e85bdd7eadc48
mejdi <mejdiclubiste@gmail.com>
2020-10-26 14:04:06 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'catfacts-client';
+  fact$: Observable<string>;
+
+  constructor(http: HttpClient) {
+    this.fact$ = http.get(
+      `${environment.apiUrl}/facts/random`,
+      { responseType: 'text' }
+    );
+  }
 }
